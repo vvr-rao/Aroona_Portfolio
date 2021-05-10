58ddea91b45bbd79190553a8d82c09d1d5e945c8
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2021-05-10 16:04:02 -0400

    Add files via upload

--- eda_hh.R
+++ eda_hh.R
@@ -34,9 +34,10 @@ points(kmeansObj$centers,col=1:3,pch=3,cex=3,lwd=3)
 kmeansObj
 #text(x+0.05,y+0.05, labels=as.character(prices$address.streetAddress))
 
-
+par(mfrow=c(1,2))
 # Kmeans cluster ONLY FOR SALE
-for_sale <- read.csv("./Househunt/Only_For_sale.csv")
+#for_sale <- read.csv("./Househunt/Only_For_sale.csv")
+for_sale <- prices[prices$homeStatus =="FOR_SALE", ]
 names(for_sale)
 
 x2 <- for_sale$livingArea
@@ -44,6 +45,7 @@ y2 <- for_sale$price
 y2 <- y2/1000
 df3 <- data.frame(x2,y2)
 dim(df3)
+df3 <- na.omit(df3)
 kmeansObj <- kmeans(df3,centers=4)
 names(kmeansObj)
 plot(x2,y2,col=kmeansObj$cluster,cex=1)
